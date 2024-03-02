fca70989d4dc63b79d11980aa15d74913e5114a1
Kartik Chandra Biswas <onsaurav@hotmail.com>
2024-03-02 12:15:08 +0600

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'kcbngtest';
+  title = 'KCB [ng] Test';
+  myText: string = "";
+  myTextWidth: number = 500;
 }
